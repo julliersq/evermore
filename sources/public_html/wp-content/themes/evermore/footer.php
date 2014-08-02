0035e0d6013446a6e27865d92401c057d120f16e
Juls <Juls@JulsLaptop>
2014-08-02 16:49:30 -0500

    hiding footer columns

--- sources/public_html/wp-content/themes/evermore/footer.php
+++ sources/public_html/wp-content/themes/evermore/footer.php
@@ -5,17 +5,43 @@
                     <div class="spans_1_of_3">
                         <div class="span_1_of_3 right">
                             <h2>Recent posts</h2>
+                            <?php
+                            $args = array(
+                                'numberposts' => 3,
+                                'category' => 2,
+                                'orderby' => 'post_date',
+                                'order' => 'DESC',
+                                'post_type' => 'post',
+                                'post_status' => 'publish',
+                                'suppress_filters' => true );                            
+                            $recentPosts = wp_get_recent_posts( $args );
+                            //echo '$recentPosts is '.print_r($recentPosts, true);
+                            ?>
                             <ul class="f_nav">
-                                <li><a href="#">Wedding photos that will blow your mind away.</a></li>
-                                <li><a href="#">intent wordpress theme features are amazing</a></li>
-                                <li><a href="#">a new era of Design is here</a></li>
-                                <li><a href="#">creative wordpress theme now available for sale.</a></li>
+                                <?php
+                                foreach($recentPosts as $currentPost){
+                                ?>
+                                <li>
+                                    <a href="/?p=<?php echo $currentPost['ID']; ?>">
+                                        <?php 
+                                        echo evermore_format_string($currentPost['post_title'], false, true, 120); 
+                                        ?>
+                                    </a>
+                                </li>
+                                <?php
+                                }
+                                ?>                                
                             </ul>
                         </div>
                         <div class="span_1_of_3 right">
-                            <h2>about intent</h2>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's  of type and scrambled it to make a type specimen book It has survived not only.</p>
-                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
+                            <?php 
+                            if( get_field( "page_excerpt", 40 ) ){
+                            ?>                           
+                            <h2>About <?php echo get_bloginfo( 'name' ); ?></h2>
+                            <?php
+                                echo get_field( "page_excerpt", 40 );
+                            }
+                            ?>                            
                         </div>
                         <div class="span_1_of_3">
                             <h2>social media</h2>
@@ -47,11 +73,24 @@
                         <div class="clear"></div>
                     </div>	
                     <div class="footer_top">
-                        <p class="link"><span>© All rights reserved | Template by&nbsp; <a href="http://w3layouts.com/"> W3Layouts</a></span></p>
+                        <p class="link"><span>©<?php date('Y'); ?> All rights reserved | <?php echo get_bloginfo( 'name' ); ?></span></p>
                     </div>
                 </div>
             </div>
         </div>
+        <script type="text/javascript">
+        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+        var disqus_shortname = 'evemoreweddings'; // required: replace example with your forum shortname
+
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function () {
+            var s = document.createElement('script'); s.async = true;
+            s.type = 'text/javascript';
+            s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+            (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+        }());
+        </script>
+
         <?php
         wp_footer();
         ?>        
